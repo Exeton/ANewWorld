dee7d73a873616ed636e35ed4eb017af743fa982
Exeton <benjamin.diederich@gmail.com>
2018-10-21 22:53:50 -0400

    Added old form1 code

--- TheNthD/Game1.cs
+++ TheNthD/Game1.cs
@@ -54,6 +54,31 @@ namespace TheNthD
 			// TODO: Add your initialization logic here
 			playerPos = new Vector2(graphics.PreferredBackBufferWidth / 2, graphics.PreferredBackBufferHeight / 2);
 			base.Initialize();
+
+			loadMap();
+			mapCacher = new ArrayMapCacher(map.GetLength(0), map.GetLength(1), map);
+			camera = new Camera(map, entities, this, mapCacher);
+
+			WindowState = FormWindowState.Maximized;
+			DoubleBuffered = true;
+
+			Bitmap playerSprite;
+			//playerSprite = new Bitmap(Bitmap.FromFile(@""));
+			playerSprite = createBox(100, 100, Color.Black);
+			player = new Player(playerSprite, 60, 200);
+			entities.Add(player);
+
+			keyManager = new KeysManager(player);
+
+			gameLoop = new Timer();
+			gameLoop.Interval = 10;
+			gameLoop.Tick += GameLoop_Tick;
+			gameLoop.Start();
+
+			keyManager.registerKeybind(Keys.F, new SaveKeybind(mapLoader));
+			keyManager.registerKeybind(Keys.R, new NewMapKeybind(mapLoader, this));
+
+			spawnSnake();
 		}
 
 		/// <summary>
@@ -101,9 +126,6 @@ namespace TheNthD
 		{
 			GraphicsDevice.Clear(Color.CornflowerBlue);
 
-
-
-
 			spriteBatch.Begin();
 
 			spriteBatch.Draw(playerSprite, playerPos, null, Color.White, 0f, playerPos, Vector2.One, SpriteEffects.None, 0f);
@@ -111,6 +133,129 @@ namespace TheNthD
 			// TODO: Add your drawing code here
 
 			base.Draw(gameTime);
+		}
+
+		private void placeBlocks()
+		{
+			if (MouseButtons == MouseButtons.Left)
+			{
+				int x = camera.toWorldX(Cursor.Position.X, (int)player.x) / 10;
+				int y = camera.toWorldY((Cursor.Position.Y - 18), (int)player.y) / 10;
+
+				map[x, y].filled = true;
+				map[x, y].color = Color.Pink;
+
+				mapCacher.invalidateRegion(x, y);
+
+			}
+		}
+
+		//Method Source https://stackoverflow.com/questions/1720160/how-do-i-fill-a-bitmap-with-a-solid-color
+		private Bitmap createBox(int width, int height, Color color)
+		{
+			Bitmap Bmp = new Bitmap(width, height);
+			using (Graphics gfx = Graphics.FromImage(Bmp))
+			using (SolidBrush brush = new SolidBrush(color))
+			{
+				gfx.FillRectangle(brush, 0, 0, width, height);
+			}
+
+			return Bmp;
+		}
+
+		private void Form1_KeyUp(object sender, KeyEventArgs e)
+		{
+			keyManager.onKeyUp(e.KeyCode);
+			setKeyValue(e.KeyCode, false);
+		}
+
+		private void Form1_KeyDown(object sender, KeyEventArgs e)
+		{
+			keyManager.onKeyDown(e.KeyCode);
+			setKeyValue(e.KeyCode, true);
+		}
+
+		private void setKeyValue(Keys keyCode, bool value)
+		{
+			switch (keyCode)
+			{
+				case Keys.W:
+					keyManager.keys[0] = value;
+					break;
+				case Keys.S:
+					keyManager.keys[1] = value;
+					break;
+				case Keys.A:
+					keyManager.keys[2] = value;
+					break;
+				case Keys.D:
+					keyManager.keys[3] = value;
+					break;
+			}
+		}
+
+		private void Form1_Paint(object sender, PaintEventArgs e)
+		{
+			camera.drawFromCenter(e.Graphics, (int)player.x, (int)player.y);
+		}
+
+		private void GameLoop_Tick(object sender, EventArgs e)
+		{
+			placeBlocks();
+			keyManager.handelInput();
+
+			ms += gameLoop.Interval;
+			int frameDelay = 1000 / fps;
+			if (ms > frameDelay)
+			{
+
+				Invalidate();
+				ms -= frameDelay;
+			}
+
+			foreach (Entity entity in entities)
+			{
+				entity.onTick(map);
+			}
+		}
+
+		private void loadMap()
+		{
+			foreach (string mapName in mapLoader.getMapNames())
+			{
+				map = mapLoader.load(mapName).onDeseralized();
+				return;
+			}
+			fillMap();
+		}
+
+		public static void fillMap()
+		{
+			Color c = Color.Brown;
+			for (int i = 0; i < 400; i++)
+				for (int j = 0; j < 200; j++)
+				{
+					bool fill = (i > 397) || (j < 2) || i < 2 || j > 197;
+					map[i, j] = new Block(fill, c);
+				}
+		}
+
+		private void spawnSnake()
+		{
+			Bitmap evilBoxBitmap = createBox(75, 75, Color.Red);
+
+			EvilBox playerTargetingBox = new EvilBox(evilBoxBitmap, 100, 100, player, 5, false);
+			entities.Add(playerTargetingBox);
+
+			Entity target = playerTargetingBox;
+
+			for (float i = 10; i > 0; i--)
+			{
+				EvilBox evilBox = new EvilBox(evilBoxBitmap, 100, 100, target, 5, true);
+				entities.Add(evilBox);
+
+				target = evilBox;
+			}
 		}
 
 
